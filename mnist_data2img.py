98b24af292bae6cda7a8a19619fa1206e669b068
Poodle Master <69660581+PoodleMaster@users.noreply.github.com>
2020-09-24 23:33:54 +0900

    Update mnist_data2img.py

--- mnist_data2img.py
+++ mnist_data2img.py
@@ -73,7 +73,7 @@ def save(data, index, num, dir):
             pix[j, i] = int(data[i,j])
     filename = dir + "/" + str(num) + "/test" + "{0:05d}".format(index) + ".png"
     img.save(filename)
-    print("\r", filename, end="")
+    print(filename)
 
 # [[  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0]
 #  [  0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0]
